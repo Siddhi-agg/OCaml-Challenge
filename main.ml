3233d541376dda27b69319c010eb06f934d2a55e
Siddhi Agrawal <101979598+Siddhi-agg@users.noreply.github.com>
2024-03-23 20:15:00 +0530

    Create main.ml and write a small differentiation code for it

---
+++ main.ml
@@ -0,0 +1,11 @@
+let rec d x = function
+  | `Int n -> `Int 0
+  | `Var y -> `Int(if x = y then 1 else 0)
+  | `Add(f, g) -> `Add(d x f, d x g)
+  | `Mul(f, g) -> `Add(`Mul(f, d x g), `Mul(g, d x f))
+  | `Pow(f, g) ->
+      `Mul(`Pow(f, `Add(g, `Int(-1))),
+           `Add(`Mul(g, d x f), `Mul(`Mul(f, `Ln f), d x g)))
+  | `Ln f -> `Pow(f, `Int(-1))
+  | `Sin f -> `Mul(d x f, `Cos f)
+  | `Cos f -> `Mul(`Int(-1), `Mul(d x f, `Sin f))
