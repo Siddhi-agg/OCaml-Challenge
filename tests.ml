3763a847e8cab34339bee4c93c2c2d29226c1548
Siddhi Agrawal <101979598+Siddhi-agg@users.noreply.github.com>
2024-03-24 20:03:15 +0530

    Added power, trignometric and logarithmic tests for derivative code

--- tests.ml
+++ tests.ml
@@ -8,7 +8,7 @@ let rec d x = function
     | `Pow(f, g) ->
         `Mul(`Pow(f, `Add(g, `Int(-1))),
              `Add(`Mul(g, d x f), `Mul(`Mul(f, `Ln f), d x g)))
-    | `Ln f -> `Pow(f, `Int(-1))
+    | `Ln f -> `Mul(`Pow(f, `Int(-1)), d x f)
     | `Sin f -> `Mul(d x f, `Cos f)
     | `Cos f -> `Mul(`Int(-1), `Mul(d x f, `Sin f))
     
@@ -27,3 +27,58 @@ let test_d_basic _ =
   let result3 = d "x" (`Add(`Var "x", `Var "y")) in
   let expected_result3 = `Add(`Int 1, `Int 0) in
   assert_equal expected_result3 result3
+
+let test_d_power _ =
+  (* Test case 4: Derivative of x^2 *)
+  let result4 = d "x" (`Mul(`Var "x", `Var "x")) in
+  let expected_result4 = `Add (`Mul (`Var "x", `Int 1), `Mul (`Var "x", `Int 1)) in
+  assert_equal expected_result4 result4;
+
+  (* Test case 5: Derivative of x^x *)
+  let result5 = d "x" (`Pow(`Var "x", `Var "x")) in
+  let expected_result5 = 
+    `Mul(
+      `Pow(`Var "x", `Add(`Var "x", `Int (-1))),
+      `Add(
+        `Mul(`Var "x", `Int 1), `Mul(`Mul(`Var "x", `Ln(`Var "x")), `Int 1)))
+  in
+  assert_equal expected_result5 result5
+
+let test_d_trigonometric _ =
+  (* Test case 6: Derivative of sin(x) *)
+  let result6 = d "x" (`Sin (`Var "x")) in
+  let expected_result6 = `Mul (`Int 1, `Cos (`Var "x")) in
+  assert_equal expected_result6 result6;
+
+  (* Test case 7: Derivative of cos(x) *)
+  let result7 = d "x" (`Cos (`Var "x")) in
+  let expected_result7 = `Mul (`Int (-1), `Mul (`Int 1, `Sin (`Var "x"))) in
+  assert_equal expected_result7 result7
+
+let test_d_logarithmic _ =
+  (* Test case 8: Derivative of ln(x) *)
+  let result8 = d "x" (`Ln (`Var "x")) in
+  let expected_result8 = `Mul(`Pow (`Var "x", `Int(-1)), `Int 1) in
+  assert_equal expected_result8 result8;
+
+  (* Test case 9: Derivative of ln(x^x) *)
+  let result9 = d "x" (`Ln (`Pow(`Var "x", `Var "x"))) in
+  let expected_result9 = 
+  `Mul
+  (`Pow(`Pow(`Var "x",`Var "x"), `Int (-1)),
+  `Mul(
+      `Pow(`Var "x", `Add(`Var "x", `Int (-1))),
+      `Add(
+        `Mul(`Var "x", `Int 1),
+        `Mul(`Mul(`Var "x", `Ln(`Var "x")), `Int 1))))
+  in
+  assert_equal expected_result9 result9
+
+  let suite =
+    "suite" >::: [
+      "test_d_basic" >:: test_d_basic;
+      "test_d_power" >:: test_d_power;
+      "test_d_trigonometric" >:: test_d_trigonometric;
+      "test_d_logarithmic" >:: test_d_logarithmic;
+    ]
+    |> run_test_tt
