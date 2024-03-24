cc3534f617bd658fe1c282307eadc979dce3f9f7
Siddhi Agrawal <101979598+Siddhi-agg@users.noreply.github.com>
2024-03-24 19:47:18 +0530

    Corrected the derivative of ln(f(x))

--- main.ml
+++ main.ml
@@ -6,6 +6,6 @@ let rec d x = function
   | `Pow(f, g) ->
       `Mul(`Pow(f, `Add(g, `Int(-1))),
            `Add(`Mul(g, d x f), `Mul(`Mul(f, `Ln f), d x g)))
-  | `Ln f -> `Pow(f, `Int(-1))
+  | `Ln f -> `Mul(`Pow(f, `Int(-1)), d x f)
   | `Sin f -> `Mul(d x f, `Cos f)
   | `Cos f -> `Mul(`Int(-1), `Mul(d x f, `Sin f))
